a9a704dbc115133e2763fb110d755404246b2ae5
erifouD <artmandr@yandex.ru>
2022-11-02 19:10:00 +0300

    Правки

--- pyTest/pyTest.py
+++ pyTest/pyTest.py
@@ -1,19 +1,13 @@
 count = int(input("Count of numbers: "))
 Arr = []
-Daymar = []
-Aberdeen = []
 for i in range(count):
     Arr.append(int(input("Number: "))) 
+print("\nLargest number: ", max(Arr), "\nSmallest number: ", min(Arr))
 for i in range(count):
     if int(Arr[i]) % 2 == 0:
-        Daymar.append(Arr[i])
-print("\nLargest number: ", max(Arr), "\nSmallest number: ", min(Arr))
-for i in range(len(Daymar)):
-    temp = str(Daymar[i])
-    tempsum = 0
-    for i in range(len(temp)):
-        tosum = int(temp[i])
-        tempsum = tempsum + tosum
-    Aberdeen.append(tempsum)
-for i in range(len(Daymar)):
-    print("\nHonest number #", i + 1, ": ", Daymar[i], " Digits sum: ", Aberdeen[i], "  Degree: ", Daymar[i] ** Daymar[i])
+        temp = str(Arr[i])
+        tempsum = 0
+        for j in range(len(temp)):
+            tosum = int(temp[j])
+            tempsum = tempsum + tosum
+        print("Honest number: ", Arr[i], "  Digits sum: ", tempsum, "Degree: ", tempsum ** tempsum)
