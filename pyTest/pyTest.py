b2244de21a18e94be1dd391f3ece91cddd5010a4
erifouD <artmandr@yandex.ru>
2022-11-03 17:49:44 +0300

    Дз четверг

--- pyTest/pyTest.py
+++ pyTest/pyTest.py
@@ -1,13 +1,7 @@
-count = int(input("Count of numbers: "))
-Arr = []
-for i in range(count):
-    Arr.append(int(input("Number: "))) 
-print("\nLargest number: ", max(Arr), "\nSmallest number: ", min(Arr))
-for i in range(count):
-    if int(Arr[i]) % 2 == 0:
-        temp = str(Arr[i])
-        tempsum = 0
-        for j in range(len(temp)):
-            tosum = int(temp[j])
-            tempsum = tempsum + tosum
-        print("Honest number: ", Arr[i], "  Digits sum: ", tempsum, "Degree: ", tempsum ** tempsum)
+print("x y z w")
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            for w in range(0, 2):
+                if not((x and not y) or (y == z) or (not w)):
+                    print(x, y, z, w)
